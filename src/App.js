4120a9431ac8385477dc7421c489bb79171e3a02
Ahmed Roshdy <a.k.roshdy@gmail.com>
2023-02-28 17:52:40 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Basic from "./Basic";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Basic />
+    </div>
+  );
+}
---
+++ src/Basic/Components/Summer/index.js
@@ -0,0 +1,48 @@
+// https://github.com/VitaliiZhukov/react-scroll-slider/blob/master/stories/Basic/Basic/Components/Summer/index.js
+import React from "react";
+import styled from "styled-components";
+
+import { ActiveItem } from "react-scroll-slider";
+
+import Content from "../Content";
+import imageUrl from "../../../assets/summer.jpg";
+
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+`;
+
+const Image = styled.div`
+  width: 500px;
+  height: 100%;
+  background-image: url(${imageUrl});
+  background-size: cover;
+  background-position: center;
+`;
+
+const forwardStyleImage = { transform: "translateY(-100%)", opacity: 0 };
+const backwardStyleImage = { transform: "translateY(100%)", opacity: 0 };
+
+const forwardStyleContent = { transform: "translateX(100%)", opacity: 0 };
+const backwardStyleContent = { transform: "translateX(-100%)", opacity: 0 };
+
+const WithImage = () => (
+  <Wrapper>
+    <ActiveItem
+      forwardStyle={forwardStyleImage}
+      backwardStyle={backwardStyleImage}
+    >
+      <Image />
+    </ActiveItem>
+
+    <ActiveItem
+      forwardStyle={forwardStyleContent}
+      backwardStyle={backwardStyleContent}
+    >
+      <Content title={"This is summer"} />
+    </ActiveItem>
+  </Wrapper>
+);
+
+export default WithImage;
